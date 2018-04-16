b0948f46574ac09e314908e6b6b938b3ec013d67
toxwow <tomasz.tomzynski@gmail.com>
2018-04-16 18:18:04 +0200

    New

---
+++ www/js/pgb.js
@@ -0,0 +1,69 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+	
+    console.log(navigator.contacts);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	
+}
+
+function deviceInfo1() {
+
+	info1 =  'Tomasz Tom¿yñski'
+
+	navigator.notification.alert(info1);
+	
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
+
+function onSuccess1(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onSuccess(contacts) {
+    alert('Found ' + contacts.length + ' contacts.');
+};
+
+function onError(contactError) {
+    alert('onError!');
+};
+
+var options      = new ContactFindOptions();
+options.filter   = "Bob";
+options.multiple = true;
+options.desiredFields = [navigator.contacts.fieldType.id];
+options.hasPhoneNumber = true;
+var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
