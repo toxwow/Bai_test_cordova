145f8d110ad71bc1aa646ee74d5193c6d1c4e601
Szajrii <michal05v@tlen.pl>
2018-04-16 19:04:55 +0200

    marian2

--- platforms/browser/www/js/pgb.js
+++ platforms/browser/www/js/pgb.js
@@ -5,21 +5,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	
     console.log(navigator.contacts);
-    
-    firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token.
-    // You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-});
+
+   
 }
 
 function deviceInfo() {
